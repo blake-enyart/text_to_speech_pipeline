c6d6c7dda81eb553caa4821b47259b3000a82ab2
Blake Enyart <blake.enyart@gmail.com>
2021-01-29 20:24:05 -0700

    feat: initial commmit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from text_to_speech_pipeline.text_to_speech_pipeline_stack import TextToSpeechPipelineStack
+
+
+app = core.App()
+TextToSpeechPipelineStack(app, "text-to-speech-pipeline", env={'region': 'us-west-2'})
+
+app.synth()
---
+++ tests/unit/test_text_to_speech_pipeline_stack.py
@@ -0,0 +1,19 @@
+import json
+import pytest
+
+from aws_cdk import core
+from text_to_speech_pipeline.text_to_speech_pipeline_stack import TextToSpeechPipelineStack
+
+
+def get_template():
+    app = core.App()
+    TextToSpeechPipelineStack(app, "text-to-speech-pipeline")
+    return json.dumps(app.synth().get_stack("text-to-speech-pipeline").template)
+
+
+def test_sqs_queue_created():
+    assert("AWS::SQS::Queue" in get_template())
+
+
+def test_sns_topic_created():
+    assert("AWS::SNS::Topic" in get_template())
